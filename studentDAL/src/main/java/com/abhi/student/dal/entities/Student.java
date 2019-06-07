6cf89208ee604c3a4d8c3dba0fc0931e05c677a8
abhis <abhis@LAPTOP-3LBD9FO7>
2019-06-07 11:24:59 +0530

    Initial commit

---
+++ studentDAL/src/main/java/com/abhi/student/dal/entities/Student.java
@@ -0,0 +1,5 @@
+package com.abhi.student.dal.entities;
+
+public class Student {
+
+}
---
+++ studentDAL/src/test/java/com/abhi/student/dal/StudentDalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abhi.student.dal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentDalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
