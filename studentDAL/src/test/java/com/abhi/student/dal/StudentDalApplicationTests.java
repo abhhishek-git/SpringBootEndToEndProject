0e56083fcebc47bacd1fa844b67b8f2a2ac0be4a
abhis <abhis@LAPTOP-3LBD9FO7>
2019-06-09 07:11:30 +0530

    Crud Operation test on Student

--- studentDAL/src/test/java/com/abhi/student/dal/StudentDalApplicationTests.java
+++ studentDAL/src/test/java/com/abhi/student/dal/StudentDalApplicationTests.java
@@ -2,15 +2,27 @@ package com.abhi.student.dal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.abhi.student.dal.entities.Student;
+import com.abhi.student.dal.repo.StudentRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentDalApplicationTests {
-
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Test
-	public void contextLoads() {
+	public void testCreateStudent() {
+		Student student = new Student();
+		student.setName("John");
+		student.setCourse("Java Web Services");
+		student.setFee(30d);
+		studentRepository.save(student);
 	}
 
 }
